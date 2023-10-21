fc3475d96c4d8843f4afa432b9f116779283b64a
Drew Wagner <33100250+Drew-Wagner@users.noreply.github.com>
2023-10-21 08:29:39 -0400

    script: toggle moving platform script

---
+++ scripts/toggle_moving_platform.gd
@@ -0,0 +1,23 @@
+extends Toggleable
+
+@export var moving_platform: MovingPlatform
+@export var on_position: Vector2
+@export var off_position: Vector2
+
+func _ready():
+	super()
+	if not moving_platform:
+		moving_platform = get_node("MovingPlatform")
+
+func _get_configuration_warnings():
+	super()
+	if not (moving_platform or get_node("MovingPlatform")):
+		return "Node requires a MovingPlatform"
+
+# Called when the element switches from off -> on
+func _on_switch_on():
+	moving_platform.move_to(on_position)
+
+# Called when the element switches from on -> off
+func _on_switch_off():
+	moving_platform.move_to(off_position)
