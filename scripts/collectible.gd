0f8d2db6f25075782b49f2fc521a37021cbb7ef6
Drew Wagner <33100250+Drew-Wagner@users.noreply.github.com>
2023-10-21 18:33:23 -0400

    bone collection sounds

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 signal collected()
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 func _ready():
 	pass
 
-
 func _on_area_2d_body_entered(body):
 	if body is MainCharacter:
+		hide()
 		collected.emit()
+
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
 		queue_free()
+
