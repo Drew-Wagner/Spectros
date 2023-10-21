a6e6909609fc7b6286bd6d0c80accab66243323d
Drew Wagner <33100250+Drew-Wagner@users.noreply.github.com>
2023-10-21 13:12:06 -0400

    start game manager

--- scripts/global_on_off.gd
+++ scripts/global_on_off.gd
@@ -3,10 +3,8 @@ extends Node
 signal toggle()
 
 var paused: bool = false
-		
 
 func _process(_delta):
 	if Input.is_action_just_pressed("toggle") or Input.is_action_just_released("toggle"):
 		if not paused:
 			toggle.emit()
-
