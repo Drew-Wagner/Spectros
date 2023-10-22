b3dac3999ffb9bc73bf13df6ff04b5cba1ce486c
Drew Wagner <33100250+Drew-Wagner@users.noreply.github.com>
2023-10-22 12:51:17 -0400

    spider ascent & descent delay

--- enemy_spider.gd
+++ enemy_spider.gd
@@ -3,6 +3,8 @@ extends Toggleable
 
 @onready var spider_body: Area2D = $SpiderBody
 
+@export var descent_delay: float = 0 # seconds
+@export var ascent_delay: float = 0 # seconds
 @export var move_speed: float = 256 # pixels / sec
 @export var move_in_editor: bool = false
 var move_direction: Vector2 = Vector2.DOWN
@@ -26,16 +28,20 @@ func _on_switch_off():
 func _physics_process(delta):
 	super(delta)
 	if not Engine.is_editor_hint() or move_in_editor:
-		if spider_body.position.length() <= delta * move_speed:
-			is_on = true
+		if move_direction == Vector2.UP and spider_body.position.length() <= delta * move_speed:
+			move_direction = Vector2.ZERO
+			await get_tree().create_timer(descent_delay).timeout
+			move_direction = Vector2.DOWN
 
-		if move_direction == Vector2.DOWN or not %RayCast2D.is_colliding():
+		if not (move_direction == Vector2.UP and %RayCast2D.is_colliding()):
 			spider_body.position += delta * move_speed * move_direction
 
 
 func _on_area_2d_body_entered(body):
 	if body.has_method("kill"):
 		body.kill()
-	else:
-		if move_direction == Vector2.DOWN:
-			is_on = false
+
+	if move_direction == Vector2.DOWN:
+		move_direction = Vector2.ZERO
+		await get_tree().create_timer(ascent_delay).timeout
+		move_direction = Vector2.UP
