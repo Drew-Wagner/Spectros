23d1def248e22813a243471b6e6ab5633dc03f83
Polywill <william.plante-tremblay@umontreal.ca>
2023-10-21 20:13:55 -0400

    Texture selectionnable sur les MovingPlatforms

--- scenes/elements/moving_platform.gd
+++ scenes/elements/moving_platform.gd
@@ -6,11 +6,25 @@ extends ToggleMovingPlatform
 		orientation = value
 		_set_orientation()
 
+@export_enum("Brick", "Wood") var material_texture: int:
+	set(value):
+		material_texture = value
+		_set_material()
+
+@export var sprite: Sprite2D
+@export var brick_texture: Texture2D
+@export var wood_texture: Texture2D
+
 enum Orientation {
 	Up,
 	Right,
 	Down,
 	Left
+}
+
+enum MaterialTexture {
+	Brick,
+	Wood
 }
 
 
@@ -27,3 +41,15 @@ func _set_orientation():
 		
 		Orientation.Left:
 			rotation = deg_to_rad(-90)
+
+
+func _set_material():
+	if sprite == null:
+		return
+
+	match material_texture:
+		MaterialTexture.Brick:
+			sprite.set_texture(brick_texture)
+		
+		MaterialTexture.Wood:
+			sprite.set_texture(wood_texture)
