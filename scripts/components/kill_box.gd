96d73720a05d5b4237bf7fe9031f07b3f73de348
Drew Wagner <33100250+Drew-Wagner@users.noreply.github.com>
2023-10-30 16:58:06 -0400

    add documentation to kill box script

--- scripts/components/kill_box.gd
+++ scripts/components/kill_box.gd
@@ -1,6 +1,18 @@
 class_name KillBox
 extends Area2D
+## Kills all killable bodies which enter it (unless in an ignored group).
+##
+## If provided a CollisionShape2D as a child,
+## will call the "kill" method on any body which enters
+## the area and is not in one of the `ignore_groups`
+##
+## NOTE: If the parent also is a PhysicsBody2D and has
+## a non-area CollisionShape2D, then make sure that that
+## shape does not prevent a body from entering this component's
+## collision shape (i.e this component's collision shape should
+## be larger than the one used for physics collisions).
 
+## Bodies in any of these groups will not be killed.
 @export var ignore_groups: Array[StringName]
 
 func _on_body_entered(body: Node2D):
